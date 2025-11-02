14e5385698d9b5a80006041a64de0b2f3e527c39
Arthur Lebron <78775633+SongXiaoYi@users.noreply.github.com>
2025-11-03 06:49:05 +0800

    Update S01.ACSM2A_knockdown.R

--- Knockdown/S02.Mature_Knockdown/S01.ACSM2A_knockdown.R
+++ Knockdown/S02.Mature_Knockdown/S01.ACSM2A_knockdown.R
@@ -31,6 +31,7 @@ countMatrix <- pseudo_Bulk__mature_normalize_copy@assays$RNA$data
 
 #countMatrix <- pseudo_Bulk__mature_normalize@assays$RNA$data
 
+########################################################### ACSM2A
 result <- scTenifoldKnk(countMatrix = countMatrix, 
                         gKO = 'ACSM2A', #需要敲除的基因
                         qc = FALSE,#是否进行QC
@@ -42,6 +43,106 @@ result <- scTenifoldKnk(countMatrix = countMatrix,
                         )
 
 setwd('H:\\CKDwork\\knokdown\\Experiment\\ACSM2A')
+save(result, file = 'ACSM2A_result_G10000_C2000.Rdata')
+
+########################################################### TPTE2
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'TPTE2', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\TPTE2')
+save(result, file = 'TPTE2_result_G10000_C2000.Rdata')
+
+########################################################### OR2L8
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'OR2L8', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\OR2L8')
+save(result, file = 'OR2L8_result_G10000_C2000.Rdata')
+gc()
+########################################################### ZNF782
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'ZNF782', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\ZNF782')
+save(result, file = 'ZNF782_result_G10000_C2000.Rdata')
+gc()
+########################################################### ZNF510
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'ZNF510', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\ZNF510')
+save(result, file = 'ZNF782_result_G10000_C2000.Rdata')
+gc()
+########################################################### CYFIP1
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'CYFIP1', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\CYFIP1')
+save(result, file = 'CYFIP1_result_G10000_C2000.Rdata')
+gc()
+########################################################### SLC35G6
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'SLC35G6', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\SLC35G6')
+save(result, file = 'SLC35G6_result_G10000_C2000.Rdata')
+gc()
+########################################################### MUC22
+result <- scTenifoldKnk(countMatrix = countMatrix, 
+                        gKO = 'MUC22', #需要敲除的基因
+                        qc = FALSE,#是否进行QC
+                        qc_mtThreshold = 0.2,#mt阈值
+                        qc_minLSize = 1000,#文库阈值(细胞测到的基因总数)
+                        nc_nNet = 10, #子网络数量
+                        nc_nCells = 500, #每个网络中随机抽取的细胞数
+                        nc_nComp = 3 #PCA 的主成分数量
+                        )
+
+setwd('H:\\CKDwork\\knokdown\\Experiment\\MUC22')
+save(result, file = 'MUC22_result_G10000_C2000.Rdata')
+gc()
 
 
 
